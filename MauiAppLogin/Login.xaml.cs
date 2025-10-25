81a8a2131778f64a57b6fd9d36bebc5a9fab83c1
Matheus Massuda <mat.massuda@gmail.com>
2025-10-25 14:05:55 -0300

    Finalização do projeto Login

--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -1,3 +1,5 @@
+using WinRT;
+
 namespace MauiAppLogin;
 
 public partial class Login : ContentPage
@@ -6,4 +8,52 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void Button_Clicked(object sender, EventArgs e )
+	{
+		try
+		{
+			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>() // Criação de lista de usuários estática (para fins didáticos)
+			{
+				new DadosUsuario()
+				{
+					Usuario = "jose",
+					Senha = "123"
+				},
+				new DadosUsuario()
+				{
+					Usuario = "maria",
+					Senha ="321"
+				}
+			};
+
+			DadosUsuario dados_digitados = new DadosUsuario() // Captura de dados digitados pelo possível usuário
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+			// LINQ
+			if(lista_usuarios.Any(
+				// Para cada item da lista será procurado um valor para usuário e senha,
+				// encontrado um usuário E a senha respectiva
+				i => (dados_digitados.Usuario == i.Usuario && 
+					 dados_digitados.Senha == i.Senha)) ) 
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+				App.Current.MainPage = new Protegida();
+
+			} else
+			{
+				throw new Exception("Usuário ou senha incorretos.");
+			}
+
+
+		} catch(Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+		}
+	}
+
 }
